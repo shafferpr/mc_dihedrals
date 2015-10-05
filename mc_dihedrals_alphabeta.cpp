430466c0647003ed72dbe1ed7f414145a9be520d
Patrick Shaffer <shafferpr@gmail.com>
2015-10-05 15:07:17 +0200

    added mc_dihedrals_alphabeta.cpp which takes a bias component that acts on alpha beta parameter

--- mc_dihedrals_alphabeta.cpp
+++ mc_dihedrals_alphabeta.cpp
@@ -2,7 +2,6 @@
 #include <random>
 #include <chrono>
 #include <armadillo>
-#include <bitset>
 
 //g++ -O2 mc_dihedrals_pt.cpp -I . -I ~/code/armadillo-4.450.2/include -DARMA_USE_BLAS -DARMA_USE_LAPACK -DARMA_DONT_USE_WRAPPER -std=c++0x -lblas -llapack -o mc_dihedrals_pt
 
@@ -13,27 +12,21 @@ int gridx=200;
 int gridy=200;
 int N_cvs=0;
 double step_size=0.1;
-double energy_ref=0.0;
 vector <double> beta(8);
 vector <double> crystal_dihedrals(50);
 vector <vector <double> > alphabeta;
-vector <vector <double> > initial_positions;
 int Nbackbone=0;
 int Nreplicas=0;
-int Nsteps=500;
-int Nsweeps=1200;
-int N_Configs=0;
+int Nsteps=8000;
+int Nsweeps=5000000;
 double kappa=0;
 double bessel=0;
-vector <vector <int>> binary_configs;
 //int Nsteps=0;
 //int Nsweeps=0;
 vector <Two_d_grid> bias_grid;
 Two_d_grid alphabetabias(200,200,8,9,0,0);
-void initialize1(string, string);
-void initialize2();
-void run_mc(int);
-void gen_initial(int);
+void initialize(string, string);
+void run_mc();
 void print(int, vector<double> &, double, double, double, double,  vector<double> &, double);
 void backbone(int, vector<double> &, vector<double> &, double);
 string outfile;
@@ -50,65 +43,21 @@ int main(int argc, char *argv[]){
     kappa = atof(argv[6]);
     bessel = atof(argv[7]);
   }
-
+  cout << "hello\n";
+  initialize(biasfilename,alphabetabiasfname);
+  cout << "hello\n";
+  run_mc();
   
-  gen_initial(N_cvs);
-  initialize1(biasfilename,alphabetabiasfname);
-  for(int i1=0; i1<N_Configs; i1++){//change upper limit back to N_Configs
-    initialize2();
-    run_mc(i1);
-  }
   return 0; 
 }
 
-
-void gen_initial(int n_dihedrals){
-  int n_configs=0;
-  n_configs=pow(2,n_dihedrals);
-  binary_configs.resize(n_configs);
-  for(int i=0; i<binary_configs.size(); i++){
-    binary_configs[i].resize(n_dihedrals);
-  }
-  
-  for(int i=0; i<n_configs; i++){
-    bitset<32> A = i;
-    for(int j=0; j<n_dihedrals; j++){
-      binary_configs[i][j]=A[n_dihedrals-1-j];
-    }
-  }
-
-  
-  binary_configs[0][0]=1;
-  binary_configs[0][1]=0;
-  binary_configs[0][2]=1;
-  binary_configs[0][3]=0;
-  binary_configs[0][4]=1;
-  binary_configs[0][5]=0;
-  binary_configs[0][6]=0;
-  binary_configs[0][7]=0;
-  binary_configs[0][8]=0;
-  binary_configs[0][9]=0;
-  binary_configs[0][10]=1;
-  binary_configs[0][11]=0;
-  binary_configs[0][12]=0;
-  binary_configs[0][13]=1;
-  binary_configs[0][14]=0;
-  binary_configs[0][15]=1;
-  binary_configs[0][16]=0;
-
-
-  N_Configs=n_configs;
-}
-
-
-void run_mc(int config_number){
+void run_mc(){
   vector <vector <double> > positions;
   double pos_temp=0;
   double energy_delta=0;
   double alphabeta1new=0;
   double alphabeta2new=0;
   vector <double> energy(Nreplicas,0);
-  vector <double> initialPos(N_cvs);
   unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
   default_random_engine generator (seed);
   uniform_real_distribution<double> real_distribution(0.0,1.0);
@@ -123,58 +72,23 @@ void run_mc(int config_number){
   double pofsnew=0;
   double pofs0=0;
   double attempts=0;
-  double rmax=0;
-  double rmin=0;
   double increment=0;
   double rx=0;
   double pofsinc=0;
-  double counter=0;
-  double partition_function=0;
-  double energyav=0;
-  double energymin=0;
-  double energyps=0;
-  double energypsav=0;
-  double energyab=0;
-  double energyabav=0;
   positions.resize(Nreplicas);
-  
-  alphabeta[0][0]=0;
-  alphabeta[0][1]=0;
   for(int k=0; k<positions.size(); k++){
     positions[k].resize(N_cvs);
     for(int j=0; j<N_cvs; j++){
-      positions[k][j]=initial_positions[j][binary_configs[config_number][j]];
-      initialPos[j]=positions[k][j];
-      rx=positions[k][j]-crystal_dihedrals[j];
-      if(j<8){
-	alphabeta[k][0] += (0.5+0.5*cos(rx));
+      if(j<15){
+	positions[k][j]=crystal_dihedrals[j]+0.3;
+	//positions[k][j]=crystal_dihedrals[j];
       }
       else{
-	alphabeta[k][1] += (0.5+0.5*cos(rx));
+	positions[k][j]=crystal_dihedrals[j];
       }
-      
     }
   }
 
-  for(int k=0; k<Nreplicas; k++){
-    energy[k]=0;
-    for(int j=0; j<Nbiases; j++){
-      energy[k]+=bias_grid[dihedral_label].getvalue_linearinterpolation(positions[k][j], positions[k][j+1]);
-    }
-    energy[k]+=alphabetabias.getvalue_linearinterpolation(alphabeta[k][0],alphabeta[k][1]);
-    energyab = alphabetabias.getvalue_linearinterpolation(alphabeta[k][0],alphabeta[k][1]);
-    pofsnew=1;
-    for(int j=0; j<N_cvs; j++){
-      pofsnew *= exp(kappa*cos(positions[k][j]-crystal_dihedrals[j]))/bessel;
-    }
-    pofsnew+=1;
-    energy[k]-=log(pofsnew)/beta[0];
-    energyps=-log(pofsnew)/beta[0];
-  }
-  
-  if(config_number==0){
-    energy_ref=energy[0];
-  }
   for(int i=0; i<=Nsweeps; i++){
     //cout << i << "\n"; 
     for(int k=0; k<Nreplicas; k++){//Nreplicas don't forget to change back
@@ -208,7 +122,7 @@ void run_mc(int config_number){
 	}
 	pofsnew+=1;
 	pofsold+=1;
-	pofsinc = (log(pofsold)-log(pofsnew))/beta[0];//beta[0] or beta[k]
+	pofsinc = (log(pofsold)-log(pofsnew))/beta[k];//beta[0] or beta[k]
 	//cout << pofsinc << " " << pofsold << " " << pofsnew << " \n";
 	for(int j1=0; j1<8; j1++){
 	  if(j1==dihedral_label){
@@ -259,7 +173,7 @@ void run_mc(int config_number){
 	//alphabeta2new=sqrt(alphabeta2new/9);
 	//cout << " " << alphabeta1new << " "<<alphabeta2new<<" "<<alphabeta[k][0]<<" "<<alphabeta[k][1]<<" "<< "\n";
 	energy_delta=0;
-
+	
 	if(dihedral_label>0 && dihedral_label< N_cvs-1){
 	  energy_delta = bias_grid[dihedral_label].getvalue_linearinterpolation(pos_temp, positions[k][dihedral_label+1])-bias_grid[dihedral_label].getvalue_linearinterpolation(positions[k][dihedral_label],positions[k][dihedral_label+1]);
 	  energy_delta += bias_grid[dihedral_label-1].getvalue_linearinterpolation(positions[k][dihedral_label-1], pos_temp)-bias_grid[dihedral_label-1].getvalue_linearinterpolation(positions[k][dihedral_label-1],positions[k][dihedral_label]);
@@ -282,15 +196,11 @@ void run_mc(int config_number){
 	  energy_delta += increment+pofsinc;
 	  //cout<<"end " << increment << " " << alphabeta1new << " "<<alphabeta2new<<" "<<alphabeta[k][0]<<" "<<alphabeta[k][1]<<" "<<increment<< "\n";
 	}
-	
-	rmax=cos(pos_temp-initialPos[dihedral_label]);
-	if((energy_delta<0 && alphabeta1new<7.9 && rmax>0.4) || (real_distribution(generator)<=exp(-beta[k]*energy_delta) && alphabeta1new<7.9 && rmax>0.4)){
+	if((energy_delta<0 && alphabeta1new<7.96 && alphabeta2new<8.96) || (real_distribution(generator)<=exp(-beta[k]*energy_delta) && alphabeta1new<7.96 && alphabeta2new<8.96)){
 	  energy[k] += energy_delta;
 	  positions[k][dihedral_label]=pos_temp;
 	  alphabeta[k][0]=alphabeta1new;
 	  alphabeta[k][1]=alphabeta2new;
-	  energyps += pofsinc;
-	  energyab += increment;
 	  if(k==0){
 	    pofs0=pofsnew;
 	  }
@@ -304,36 +214,37 @@ void run_mc(int config_number){
 	}
       }
     }
-    if(i==0){
-      energymin=energy[0];
-    }
-    if(i%1==0){
+    
+    if(i%2==0){
       //cout << "hello\n";
-      //backbone(i,positions[0],energy,pofs0);
-      partition_function+=exp(-(energy[0]-energy_ref)*beta[0]);
-      energyav+=energy[0];
-      energypsav+=energyps;
-      energyabav+=energyab;
-      if(energy[0]<energymin){
-	energymin=energy[0];
-      }
-      counter+=1;
-      //cout << alphabeta[0][0] << " " << alphabeta[0][1]<< "\n";
+      backbone(i,positions[0],energy,pofs0);
     }
     
+    //swap_pair=distributionB(generator);
+    //double bf = (beta[swap_pair]-beta[swap_pair+1])*(energy[swap_pair+1]-energy[swap_pair]);
+    
+    /*if(bf>0 || real_distribution(generator)<exp(bf)){
+      for(int k=0; k<N_cvs; k++){
+	double tmp1=positions[swap_pair][k];
+	double tmp2=positions[swap_pair+1][k];
+	positions[swap_pair+1][k]=tmp1;
+	positions[swap_pair][k]=tmp2;	
+      }
+      for(int k=0; k<2; k++){
+	double tmp1=alphabeta[swap_pair][k];
+	double tmp2=alphabeta[swap_pair+1][k];
+	alphabeta[swap_pair+1][k]=tmp1;
+	alphabeta[swap_pair][k]=tmp2;		
+      }
+      double tmp1=energy[swap_pair];
+      double tmp2=energy[swap_pair+1];
+      energy[swap_pair+1]=tmp1;
+      energy[swap_pair]=tmp2;
+      }*/
   }
-  
-  partition_function = -log(partition_function);
-  energyav=energyav/counter;
-  energypsav=energypsav/counter;
-  energyabav=energyabav/counter;
-  cout << config_number << " " << partition_function << " " << energyav << "\n";
-  ofstream colvarfile;
-  colvarfile.open(outfile, ios_base::app);
-  colvarfile << config_number <<" " << partition_function << " " << energyav << " " << energymin << " " << energypsav << " " << energyabav << "\n";
-  colvarfile.close();
+
 }
-  
+
 void backbone(int counter, vector<double> & allpositions, vector<double> & allenergies, double pofs0){
   vector< vector <double> > backbone;
   vector< double > bond_distances(Nbackbone+8);
@@ -348,7 +259,7 @@ void backbone(int counter, vector<double> & allpositions, vector<double> & allen
   for(int k=0; k<Nbackbone; k++){
     backbone[k].resize(3);
   }
-  /* backbone[0][0]=0;backbone[0][1]=0;backbone[0][2]=0;
+  backbone[0][0]=0;backbone[0][1]=0;backbone[0][2]=0;
   bond_distances[0]=0.133386;
   for(int k=0; k<=N_cvs/2+2; k++){
     bond_distances[3*k]=0.147101;
@@ -444,13 +355,13 @@ void backbone(int counter, vector<double> & allpositions, vector<double> & allen
   dihedral_angles[16]=-3.0913;
   dihedral_angles[19]=-3.1074;
   dihedral_angles[22]=2.9107;
-  dihedral_angles[25]=2.9367;*/
+  dihedral_angles[25]=2.9367;
 
   /*for(int k=0; k<=25; k++){
     cout << dihedral_angles[k] << "\n";
     }*/
 
-  /*
+  
   vec terminal_vector(4);
   mat transformation_matrix(4,4,fill::eye);
   mat new_matrix(4,4,fill::eye);
@@ -469,12 +380,12 @@ void backbone(int counter, vector<double> & allpositions, vector<double> & allen
   transformation_matrix=transformation_matrix*new_matrix;
   backbone[2][0]=transformation_matrix(0,3);
   backbone[2][1]=transformation_matrix(1,3);
-  backbone[2][2]=transformation_matrix(2,3);*/
+  backbone[2][2]=transformation_matrix(2,3);
 
   //backbonefile <<"N "<<10*backbone[0][0] <<" "<<10*backbone[0][1]<<" "<<10*backbone[0][2]<<"\n";
   //backbonefile <<"C "<<10*backbone[1][0] <<" "<<10*backbone[1][1]<<" "<<10*backbone[1][2]<<"\n";
   //backbonefile <<"C "<<10*backbone[2][0] <<" "<<10*backbone[2][1]<<" "<<10*backbone[2][2]<<"\n";
-  /*double cos_ba=0; double sin_ba=0;
+  double cos_ba=0; double sin_ba=0;
   double cos_da=0; double sin_da=0;
   for(int k=3; k<Nbackbone; k++){
     cos_ba=cos(bond_angles[k-2]); sin_ba=sin(bond_angles[k-2]);
@@ -490,17 +401,17 @@ void backbone(int counter, vector<double> & allpositions, vector<double> & allen
     backbone[k][1]=transformation_matrix(1,3);
     backbone[k][2]=transformation_matrix(2,3);
 
-    //if(k%3==0){
-      //backbonefile <<"N " <<10*backbone[k][0] <<" "<<10*backbone[k][1]<<" "<<10*backbone[k][2]<<"\n";
-    //}
-    //else{
-      //backbonefile <<"C " <<10*backbone[k][0] <<" "<<10*backbone[k][1]<<" "<<10*backbone[k][2]<<"\n";
-      //}
+    /*if(k%3==0){
+      backbonefile <<"N " <<10*backbone[k][0] <<" "<<10*backbone[k][1]<<" "<<10*backbone[k][2]<<"\n";
+    }
+    else{
+      backbonefile <<"C " <<10*backbone[k][0] <<" "<<10*backbone[k][1]<<" "<<10*backbone[k][2]<<"\n";
+      }*/
   
-  }*/
+  }
 
 
-  /*double rx, ry, rz=0;
+  double rx, ry, rz=0;
   rx=backbone[28][0]-backbone[1][0];
   ry=backbone[28][1]-backbone[1][1];
   rz=backbone[28][2]-backbone[1][2];
@@ -519,9 +430,9 @@ void backbone(int counter, vector<double> & allpositions, vector<double> & allen
   rg += sqrt(pow(backbone[22][0]-meanx,2)+pow(backbone[22][1]-meany,2)+pow(backbone[22][2]-meanz,2));
   rg += sqrt(pow(backbone[25][0]-meanx,2)+pow(backbone[25][1]-meany,2)+pow(backbone[25][2]-meanz,2));
   rg += sqrt(pow(backbone[28][0]-meanx,2)+pow(backbone[28][1]-meany,2)+pow(backbone[28][2]-meanz,2));
-  rg = rg/10;*/
+  rg = rg/10;
   
-  double rx=0;
+  rx=0;
   dihedral_alphabeta2=0; dihedral_alphabeta1=0;
   for( int k=0; k<N_cvs; k++){
     rx=allpositions[k]-crystal_dihedrals[k];
@@ -542,8 +453,8 @@ void backbone(int counter, vector<double> & allpositions, vector<double> & allen
   }
   //dihedral_alphabeta1=sqrt(dihedral_alphabeta1/8);
   //dihedral_alphabeta2=sqrt(dihedral_alphabeta2/9);
-  double r2=0; double rg=0;
-  //print(counter, allpositions, r2, rg, dihedral_alphabeta1, dihedral_alphabeta2, allenergies, pofs0);
+
+  print(counter, allpositions, r2, rg, dihedral_alphabeta1, dihedral_alphabeta2, allenergies, pofs0);
   //cout << "a" << dihedral_alphabeta1 <<" "<< alphabeta[0][0]  << "\n";
   //cout << "b" << dihedral_alphabeta2 <<" "<< alphabeta[0][1]  << "\n";
   //cout << "hello 7\n";
@@ -551,7 +462,7 @@ void backbone(int counter, vector<double> & allpositions, vector<double> & allen
 }
 
 
-void initialize1(string biasfname, string alphabetabiasfname){
+void initialize(string biasfname, string alphabetabiasfname){
   Two_d_grid gridtemplate(gridx, gridy, 2*3.141592654, 2*3.141592654, -3.141592654, -3.141592654);
   //Two_d_grid gridtemplateb(gridx, gridy, 8, 10, 0, 0);
 //Nbiases is the number of 2d_grids you want to make, gridx, and gridy are the number of gridpoints you are using
@@ -567,19 +478,43 @@ void initialize1(string biasfname, string alphabetabiasfname){
   alphabetabias.setindices();
   //cout << "index set\n";
   Nbackbone=3*N_cvs/2 +5;
-  Nreplicas=1;
+  Nreplicas=5;
   beta[0]=0.353742;
-  //ofstream colvarfile;
+  beta[1]=0.3306;
+  beta[2]=0.3089;
+  beta[3]=0.2887;
+  beta[4]=0.2698;
+  beta[5]=0.2552;
+  beta[6]=0.2357;
+  beta[7]=0.2202;
+  ofstream colvarfile;
   //colvarfile.open("colvar_pt5rep.data", ios_base::app);
-  //colvarfile.open(outfile, ios_base::app);
-  //colvarfile <<"#! FIELDS time psi-1 phi-2 psi-2 phi-3 psi-3 psi-4 phi-5 psi-5 phi-6 psi-6 phi-7 psi-7 phi-8 psi-8 phi-9 psi-9 phi-10 d1 rg dihedral_alphabeta1 dihedral_alphabeta2 bias"<<"\n";
-  //colvarfile.close();
+  colvarfile.open(outfile, ios_base::app);
+  colvarfile <<"#! FIELDS time psi-1 phi-2 psi-2 phi-3 psi-3 psi-4 phi-5 psi-5 phi-6 psi-6 phi-7 psi-7 phi-8 psi-8 phi-9 psi-9 phi-10 d1 rg dihedral_alphabeta1 dihedral_alphabeta2 bias"<<"\n";
+  colvarfile.close();
+  /*crystal_dihedrals[0]=2.41;
+  crystal_dihedrals[1]=-2.52;
+  crystal_dihedrals[2]=1.98;
+  crystal_dihedrals[3]=-1.58;
+  crystal_dihedrals[4]=1.98;
+  crystal_dihedrals[5]=-0.26;
+  crystal_dihedrals[6]=-0.89011;
+  crystal_dihedrals[7]=-1.1519;
+  crystal_dihedrals[8]=-1.5882;
+  crystal_dihedrals[9]=-0.36651;
+  crystal_dihedrals[10]=1.4486;
+  crystal_dihedrals[11]=0.078;
+  crystal_dihedrals[12]=-1.186;
+  crystal_dihedrals[13]=1.867;
+  crystal_dihedrals[14]=-1.308;
+  crystal_dihedrals[15]=2.356;
+  crystal_dihedrals[16]=-2.635;*/
+  
   crystal_dihedrals[0]=2.41;
   crystal_dihedrals[1]=-2.0;
   crystal_dihedrals[2]=2.3;
   crystal_dihedrals[3]=-1.58;
   crystal_dihedrals[4]=1.98;
-  // crystal_dihedrals[5]=-1.34;
   crystal_dihedrals[5]=-0.26;
   crystal_dihedrals[6]=-1.3;
   crystal_dihedrals[7]=-0.8;
@@ -593,55 +528,36 @@ void initialize1(string biasfname, string alphabetabiasfname){
   crystal_dihedrals[15]=2.356;
   crystal_dihedrals[16]=-2.0;
 
-  initial_positions.resize(N_cvs);
-  for(int k=0; k<N_cvs; k++){
-    initial_positions[k].resize(2);
-  }
-
-  initial_positions[0][0]=-1.5;
-  initial_positions[0][1]=3.1;
-  initial_positions[1][0]=-1.5;
-  initial_positions[1][1]=1.2;
-  initial_positions[2][0]=-0.3;
-  initial_positions[2][1]=2.5;
-  initial_positions[3][0]=-1.5;
-  initial_positions[3][1]=1.15;
-  initial_positions[4][0]=-0.7;
-  initial_positions[4][1]=2.4;
-  initial_positions[5][0]=-0.3;
-  initial_positions[5][1]=2.4;
-  initial_positions[6][0]=-1.4;
-  initial_positions[6][1]=1.1;
-  initial_positions[7][0]=-0.2;
-  initial_positions[7][1]=2.4;
-  initial_positions[8][0]=-1.5;
-  initial_positions[8][1]=1.1;
-  initial_positions[9][0]=-0.3;
-  initial_positions[9][1]=2.1;
-  initial_positions[10][0]=-1.5;
-  initial_positions[10][1]=1.3;
-  initial_positions[11][0]=0.0;
-  initial_positions[11][1]=3.0;
-  initial_positions[12][0]=-1.5;
-  initial_positions[12][1]=1.2;
-  initial_positions[13][0]=-0.3;
-  initial_positions[13][1]=2.2;
-  initial_positions[14][0]=-1.4;
-  initial_positions[14][1]=1.2;
-  initial_positions[15][0]=-0.3;
-  initial_positions[15][1]=2.3;
-  initial_positions[16][0]=-1.4;
-  initial_positions[16][1]=1.1;
 
   alphabeta.resize(Nreplicas);
   for(int k=0; k<alphabeta.size(); k++){
     alphabeta[k].resize(2);
   }
-
-}
-
-void initialize2(){
   double rx=0;
+  for(int j=0; j<Nreplicas; j++){
+    for(int k=0; k<N_cvs; k++){
+      rx=0-crystal_dihedrals[k];
+      rx=0;//start from crystal structure
+      if(k<15){
+	rx=0.3;
+      }
+      if(k<8){
+	//dihedral_rmsd1+=rx*rx;
+	alphabeta[j][0]+=(0.5+0.5*cos(rx));
+	//alphabeta[j][0]+=rx*rx;
+      }
+      else{
+	//dihedral_rmsd2+=rx*rx;
+	alphabeta[j][1]+=(0.5+0.5*cos(rx));
+	//alphabeta[j][1]+=rx*rx;
+      }
+    }
+    //alphabeta[j][0]=sqrt(alphabeta[j][0]/8);
+    //alphabeta[j][1]=sqrt(alphabeta[j][1]/9);
+    //alphabeta[j][0]+=1;
+    cout << alphabeta[j][0] << " " << alphabeta[j][1]<< "\n";
+  }
+  
 
 }
 
